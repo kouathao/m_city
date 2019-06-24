dd0028be929064044d26aa4fb947ab85afe2c045
Koua Thao <kouathao09@gmail.com>
2019-06-24 00:39:22 -0500

    completed section 37

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,12 +3,14 @@ import React from "react";
 // components
 import Featured from "./featured";
 import Matches from "./matches";
+import MeetPlayers from "./meetPlayers";
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
       <Matches />
+      <MeetPlayers />
     </div>
   );
 };
