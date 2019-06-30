f6e2b951e4e76979b5dfc943bec9770a666e3534
Koua Thao <kouathao09@gmail.com>
2019-06-30 15:36:31 -0500

    completed section 45

---
+++ src/components/admin/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminLayout from "../../Hoc/AdminLayout";
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <h1>This is your dashboard</h1>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,14 @@ import Layout from "./Hoc/Layout";
 import Home from "./components/home";
 import SignIn from "./components/signin";
 
+// Admin Routes
+import Dashboard from "./components/admin/Dashboard";
+
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
